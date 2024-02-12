f9f2c0e46613af27437b6b341d438959d53045e4
JamisonSavellano <158102826+JamisonSavellano@users.noreply.github.com>
2024-02-12 13:52:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 $(document).ready(function() {
-	
+	$("#q1").click(function{
+		$("#a1").toggle();
+	});
+	$("#q2").click(function{
+		$("#a2").toggle();
+	});
+	$("#q3").click(function{
+			$("#a3").toggle();
+	});
+	$("#q4").click(function{
+			$("#a4").toggle();
+	});
+	$("#q5").click(function{
+			$("#a5").toggle();
+	});
+	$("#q6").click(function{
+			$("#a6").toggle();
+	});
+	$("#q7").click(function{
+			$("#a7").toggle();
+	});
+	$("#q8").click(function{
+			$("#a8").toggle();
+	});
+	$("#q9").click(function{
+			$("#a9").toggle();
+	});
+	$("#q10").click(function{
+			$("#a10").toggle();
+	});
 });
