2556f67ef0f7f7921f318373cd7237707e71b508
JamisonSavellano <158102826+JamisonSavellano@users.noreply.github.com>
2024-02-13 13:31:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,30 +3,30 @@ $(document).ready(function() {
 		$("#a1").css("color": "black");
 	});
 	$("#q2").click(function(){
-		$("#a2").css();
+		$("#a2").css("color": "black");
 	});
 	$("#q3").click(function(){
-			$("#a3").css();
+			$("#a3").css("color": "black");
 	});
 	$("#q4").click(function(){
-			$("#a4").css();
+			$("#a4").css("color": "black");
 	});
 	$("#q5").click(function(){
-			$("#a5").css();
+			$("#a5").css("color": "black");
 	});
 	$("#q6").click(function(){
-			$("#a6").css();
+			$("#a6").css("color": "black");
 	});
 	$("#q7").click(function(){
-			$("#a7").css();
+			$("#a7").css("color": "black");
 	});
 	$("#q8").click(function(){
-			$("#a8").css();
+			$("#a8").css("color": "black");
 	});
 	$("#q9").click(function(){
-			$("#a9").css();
+			$("#a9").css("color": "black");
 	});
 	$("#q10").click(function(){
-			$("#a10").css();
+			$("#a10").css("color": "black");
 	});
 });
